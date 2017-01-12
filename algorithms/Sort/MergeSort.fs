91513d947e5b023bd52226a0cc483f8d8177fa96
Miguel Sotolongo <Miguel Sotolongo>
2017-01-12 00:04:44 -0800

    Tested linking of F# dll with C# program.

---
+++ algorithms/Sort/MergeSort.fs
@@ -0,0 +1,7 @@
+﻿namespace Algorithms
+
+module Sort =
+    let rec fib = function
+        | n when n=0I -> 0I
+        | n when n=1I -> 1I
+        | n -> fib(n-1I) + fib(n-2I)
---
+++ algorithms/Tests/SortTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class SortTests
+    {
+        [TestMethod]
+        public void WhenEmptyInput_ExpectEmptyReturn()
+        {
+            Console.WriteLine(Algorithms.Sort.fib(10));
+        }
+    }
+}
